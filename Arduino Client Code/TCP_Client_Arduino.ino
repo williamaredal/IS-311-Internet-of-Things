5d77ae4f81a1d58af2f2985767db15f6f4d08c42
williamaredal <williamaredal@hotmail.com>
2023-09-28 15:50:09 +0200

    Latest version of Arduino TCP client

--- Arduino Client Code/TCP_Client_Arduino.ino
+++ Arduino Client Code/TCP_Client_Arduino.ino
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 
 
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 #include <Wire.h>
 
 #include <WiFiS3.h>  
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 
-#define MAX 100
 
 const int microphonePin = A0; // Analog pin where the microphone is connected
 
@@ -17,10 +20,29 @@ const int server_port = 12345;            // the port number
 
 WiFiClient client;  // Create a WiFiClient object
 
+Adafruit_MPU6050 mpu;
+
+SoftwareSerial gpsSerial(2, 3); // RXD pin 2, TXD pin 3
+TinyGPSPlus gps;
 
 void setup() {
-  Serial.begin(115200); // Initialize serial communication
+  Serial.begin(9600); // Initialize serial communication
+  gpsSerial.begin(9600); // GPS module communication
 
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+  // set accelerometer range to +-8G
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  // set gyro range to +- 500 deg/s
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  // set filter bandwidth to 21 Hz
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  delay(100);
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -32,87 +54,150 @@ void setup() {
 }
 
 void loop() {
+  // Serial.println("Loop");
+  while (gpsSerial.available() > 0) {
+    if (gps.encode(gpsSerial.read())) {
+      if (gps.location.isUpdated()) {
 
-   /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
-  /* Print out the readings */
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
-  Serial.print("Temperature: ");
-  Serial.print(temp.temperature);
-  Serial.println(" degC");
-  Serial.println("");
+        Serial.println("------------------");
+        Serial.print("Latitude: ");
+        Serial.println(gps.location.lat(), 6);
+        Serial.print("Longitude: ");
+        Serial.println(gps.location.lng(), 6);
+        Serial.print("Altitude: ");
+        Serial.println(gps.altitude.meters(), 6);
+        Serial.print("Course: ");
+        Serial.println(gps.course.deg(), 6);
+        Serial.print("Speed: ");
+        Serial.println(gps.speed.kmph(), 6);
+        Serial.print("Satellites: ");
+        Serial.println(gps.satellites.value(), 6);
 
+         if (gps.date.isValid() && gps.time.isValid()) {
+          Serial.print("Date: ");
+          Serial.print(gps.date.year());
+          Serial.print("-");
+          Serial.print(gps.date.month());
+          Serial.print("-");
+          Serial.print(gps.date.day());
+          Serial.print(" Time: ");
+          Serial.print(gps.time.hour());
+          Serial.print(":");
+          Serial.print(gps.time.minute());
+          Serial.print(":");
+          Serial.print(gps.time.second());
+          Serial.println();
+        }
+       
 
-  // Read the analog value from the microphone
-  float microphoneValue = analogRead(microphonePin);
-
-  // Print the microphone value to the serial monitor
-  Serial.println(microphoneValue);
-
-  if (client.connect(server_ip, server_port)) {
-    Serial.println("Connected to server");
-    // client.write("hello");  // Send the message to the server
-
-    // Convert the floats to byte arrays
-    byte x_bytes[sizeof(float)];
-    byte y_bytes[sizeof(float)];
-    byte z_bytes[sizeof(float)];
-    byte gyro_x_bytes[sizeof(float)];
-    byte gyro_y_bytes[sizeof(float)];
-    byte gyro_z_bytes[sizeof(float)];
-    byte temp_bytes[sizeof(float)];
-
-    /*
-    memcpy(x_bytes, &a.acceleration.x, sizeof(float));
-    memcpy(y_bytes, &a.acceleration.y, sizeof(float));
-    memcpy(z_bytes, &a.acceleration.z, sizeof(float));
-    memcpy(gyro_x_bytes, &g.gyro.x, sizeof(float));
-    memcpy(gyro_y_bytes, &g.gyro.y, sizeof(float));
-    memcpy(gyro_z_bytes, &g.gyro.z, sizeof(float));
-    memcpy(temp_bytes, &temp.temperature, sizeof(float));
-    */
-    memcpy(, &g.gyro.x, sizeof(float));
-    memcpy(, &g.gyro.y, sizeof(float));
-    memcpy(, &g.gyro.z, sizeof(float));
-    memcpy(, &temp.temperature, sizeof(float));
-
-    // Send the byte arrays to the server
-    /*
-    client.write(x_bytes, sizeof(float));
-    client.write(y_bytes, sizeof(float));
-    client.write(z_bytes, sizeof(float));
-    client.write(gyro_x_bytes, sizeof(float));
-    client.write(gyro_y_bytes, sizeof(float));
-    client.write(gyro_z_bytes, sizeof(float));
-    client.write(temp_bytes, sizeof(float));
-    */
+        /* Get new sensor events with the readings */
+        sensors_event_t a, g, temp;
+        mpu.getEvent(&a, &g, &temp);
+        /* Print out the readings */
+        Serial.print("Acceleration X: ");
+        Serial.print(a.acceleration.x);
+        Serial.print(", Y: ");
+        Serial.print(a.acceleration.y);
+        Serial.print(", Z: ");
+        Serial.print(a.acceleration.z);
+        Serial.println(" m/s^2");
+        Serial.print("Rotation X: ");
+        Serial.print(g.gyro.x);
+        Serial.print(", Y: ");
+        Serial.print(g.gyro.y);
+        Serial.print(", Z: ");
+        Serial.print(g.gyro.z);
+        Serial.println(" rad/s");
+        Serial.print("Temperature: ");
+        Serial.print(temp.temperature);
+        Serial.println(" degC");
 
 
-    byte mic_bytes[sizeof(float)];
+        // Read the analog value from the microphone
+        float microphoneValue = analogRead(microphonePin);
 
-    memcpy(mic_bytes, &microphoneValue, sizeof(float));
+        // Print the microphone value to the serial monitor
+        Serial.print("Microphone: ");
+        Serial.println(microphoneValue);
 
-    // Send the byte arrays to the server
-    client.write(mic_bytes, sizeof(float));
+        if (client.connect(server_ip, server_port)) {
+          Serial.println("Connected to server");
+          // client.write("hello");  // Send the message to the server
 
-    client.stop();  // Disconnect from the server
-    delay(100);
-  } else {
-    Serial.println("Connection failed");
-    delay(5000);    // Wait for 5 seconds before retrying
+          // Convert the floats to byte arrays
+          byte x_bytes[sizeof(float)];
+          byte y_bytes[sizeof(float)];
+          byte z_bytes[sizeof(float)];
+          byte gyro_x_bytes[sizeof(float)];
+          byte gyro_y_bytes[sizeof(float)];
+          byte gyro_z_bytes[sizeof(float)];
+          byte temp_bytes[sizeof(float)];
+
+          memcpy(x_bytes, &a.acceleration.x, sizeof(float));
+          memcpy(y_bytes, &a.acceleration.y, sizeof(float));
+          memcpy(z_bytes, &a.acceleration.z, sizeof(float));
+          memcpy(gyro_x_bytes, &g.gyro.x, sizeof(float));
+          memcpy(gyro_y_bytes, &g.gyro.y, sizeof(float));
+          memcpy(gyro_z_bytes, &g.gyro.z, sizeof(float));
+          memcpy(temp_bytes, &temp.temperature, sizeof(float));
+
+          // Send the byte arrays to the server
+          client.write(x_bytes, sizeof(float));
+          client.write(y_bytes, sizeof(float));
+          client.write(z_bytes, sizeof(float));
+          client.write(gyro_x_bytes, sizeof(float));
+          client.write(gyro_y_bytes, sizeof(float));
+          client.write(gyro_z_bytes, sizeof(float));
+          client.write(temp_bytes, sizeof(float));
+
+
+          byte mic_bytes[sizeof(float)];
+
+          memcpy(mic_bytes, &microphoneValue, sizeof(float));
+
+          // Send the byte arrays to the server
+          client.write(mic_bytes, sizeof(float));
+
+          // Store the values in variables
+          double latitude = gps.location.lat();
+          double longitude = gps.location.lng();
+          double altitude = gps.altitude.meters();
+          double course = gps.course.deg();
+          double speed = gps.speed.kmph();
+          uint32_t satellites = gps.satellites.value();
+
+          // Convert the variables to byte arrays
+          byte lat_bytes[sizeof(double)];
+          byte long_bytes[sizeof(double)];
+          byte alt_bytes[sizeof(double)];
+          byte course_bytes[sizeof(double)];
+          byte speed_bytes[sizeof(double)];
+          byte sat_bytes[sizeof(uint32_t)];
+
+          memcpy(lat_bytes, &latitude, sizeof(double));
+          memcpy(long_bytes, &longitude, sizeof(double));
+          memcpy(alt_bytes, &altitude, sizeof(double));
+          memcpy(course_bytes, &course, sizeof(double));
+          memcpy(speed_bytes, &speed, sizeof(double));
+          memcpy(sat_bytes, &satellites, sizeof(uint32_t));
+
+
+          // Send the byte arrays to the server
+          client.write(lat_bytes, sizeof(double));
+          client.write(long_bytes, sizeof(double));
+          client.write(alt_bytes, sizeof(double));
+          client.write(course_bytes, sizeof(double));
+          client.write(speed_bytes, sizeof(double));
+          client.write(sat_bytes, sizeof(uint32_t));
+
+          client.stop();  // Disconnect from the server
+          // delay(10);
+        } else {
+          Serial.println("Connection failed");
+          delay(5000);    // Wait for 5 seconds before retrying
+        }
+
+      }
+    }
   }
 }
