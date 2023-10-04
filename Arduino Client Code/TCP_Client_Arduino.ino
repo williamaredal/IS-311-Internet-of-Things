ed6655469d173e9d6764bdbc04ea6317e5da5470
williamaredal <williamaredal@hotmail.com>
2023-10-04 13:25:53 +0200

    refactoring of Arduino TCP Client

--- Arduino Client Code/TCP_Client_Arduino.ino
+++ Arduino Client Code/TCP_Client_Arduino.ino
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
@@ -10,13 +9,13 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
-
-const int microphonePin = A0; // Analog pin where the microphone is connected
-
 const char* ssid = "";      // your Wi-Fi network SSID (network name)
 const char* password = "";  // your Wi-Fi password
 const char* server_ip = "";  // use ifconfig to identify mac ip by finding 'en0', and its 'inet' ip address 
 const int server_port = 12345;            // the port number
+
+const int microphonePin = A0; // Analog pin where the microphone is connected
+int microphoneValue;
 
 WiFiClient client;  // Create a WiFiClient object
 
@@ -25,10 +24,46 @@ Adafruit_MPU6050 mpu;
 SoftwareSerial gpsSerial(2, 3); // RXD pin 2, TXD pin 3
 TinyGPSPlus gps;
 
-void setup() {
-  Serial.begin(9600); // Initialize serial communication
-  gpsSerial.begin(9600); // GPS module communication
+sensors_event_t a, g, temp;
 
+// Calibrated offsets (replace with your own values)
+float accelXOffset = 0.2;
+float accelYOffset = -0.17;
+float accelZOffset = 4.92335;
+
+float gyroXOffset = -0.08;
+float gyroYOffset = 0.00;
+float gyroZOffset = -0.04;
+
+float calibratedAccelX;
+float calibratedAccelY;
+float calibratedAccelZ;
+
+float calibratedGyroX;
+float calibratedGyroY;
+float calibratedGyroZ;
+
+
+// Declare global variables for GPS values
+double latitude = 0.0;
+double longitude = 0.0;
+double altitude = 0.0;
+double course = 0.0;
+double speed = 0.0;
+int satellites = 0;
+
+bool areValidValues = false;
+
+
+void setup() {
+  Serial.begin(115200); // Initialize serial communication
+  initMpu();
+  initGps();
+  initWifi();
+
+}
+
+void initMpu() {
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
@@ -42,8 +77,15 @@ void setup() {
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   // set filter bandwidth to 21 Hz
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  delay(100);
 
+  delay(100);
+}
+
+void initGps() {
+  gpsSerial.begin(9600); // GPS module communication
+}
+
+void initWifi() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -54,150 +96,156 @@ void setup() {
 }
 
 void loop() {
-  // Serial.println("Loop");
+
+  Serial.println("-----------------------------");
+
+  getMpu();
+  getMic();
+  getGps();
+
+  if (areValidValues) {
+    printValues();
+    sendData();
+  }
+
+  areValidValues = 0;
+
+  delay(100);
+
+       
+}
+
+void printValues() {
+  // Print the calibrated mpu values
+  Serial.print("Calibrated Acceleration X: ");
+  Serial.print(calibratedAccelX);
+  Serial.print(", Y: ");
+  Serial.print(calibratedAccelY);
+  Serial.print(", Z: ");
+  Serial.print(calibratedAccelZ);
+  Serial.println(" m/s^2");
+
+  Serial.print("Calibrated Rotation X: ");
+  Serial.print(calibratedGyroX);
+  Serial.print(", Y: ");
+  Serial.print(calibratedGyroY);
+  Serial.print(", Z: ");
+  Serial.print(calibratedGyroZ);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  // Print the mic values
+  Serial.print("Microphone: ");
+  Serial.println(microphoneValue);
+
+  // Print the gps values 
+  Serial.print("Latitude: ");
+  Serial.println(latitude, 6);
+  Serial.print("Longitude: ");
+  Serial.println(longitude, 6);
+  Serial.print("Altitude: ");
+  Serial.println(altitude, 6);
+  Serial.print("Course: ");
+  Serial.println(course, 6);
+  Serial.print("Speed: ");
+  Serial.println(speed, 6);
+  Serial.print("Satellites: ");
+  Serial.println(satellites, 6);
+  Serial.print("Valid?: ");
+  Serial.println(areValidValues);
+}
+
+void getMic() {
+  int mn = 1024;
+  int mx = 0;
+
+  for (int i = 0; i < 10; ++i) {
+    int val = analogRead(microphonePin);
+    mn = min(mn, val);
+    mx = max(mx, val);
+  }
+
+  int delta = mx - mn;
+  microphoneValue = delta;
+}
+
+void getMpu() {
+  mpu.getEvent(&a, &g, &temp);
+
+  // Apply offset compensation and create variables for calibrated values
+  calibratedAccelX = a.acceleration.x - accelXOffset;
+  calibratedAccelY = a.acceleration.y - accelYOffset;
+  calibratedAccelZ = a.acceleration.z - accelZOffset;
+
+  calibratedGyroX = g.gyro.x - gyroXOffset;
+  calibratedGyroY = g.gyro.y - gyroYOffset;
+  calibratedGyroZ = g.gyro.z - gyroZOffset;
+}
+
+void getGps() {
   while (gpsSerial.available() > 0) {
     if (gps.encode(gpsSerial.read())) {
-      if (gps.location.isUpdated()) {
+      
+      areValidValues = gps.location.isValid() && gps.course.isValid() && gps.altitude.isValid() && gps.speed.isValid() && gps.satellites.isValid();
 
-        Serial.println("------------------");
-        Serial.print("Latitude: ");
-        Serial.println(gps.location.lat(), 6);
-        Serial.print("Longitude: ");
-        Serial.println(gps.location.lng(), 6);
-        Serial.print("Altitude: ");
-        Serial.println(gps.altitude.meters(), 6);
-        Serial.print("Course: ");
-        Serial.println(gps.course.deg(), 6);
-        Serial.print("Speed: ");
-        Serial.println(gps.speed.kmph(), 6);
-        Serial.print("Satellites: ");
-        Serial.println(gps.satellites.value(), 6);
+      if (areValidValues) {
 
-         if (gps.date.isValid() && gps.time.isValid()) {
-          Serial.print("Date: ");
-          Serial.print(gps.date.year());
-          Serial.print("-");
-          Serial.print(gps.date.month());
-          Serial.print("-");
-          Serial.print(gps.date.day());
-          Serial.print(" Time: ");
-          Serial.print(gps.time.hour());
-          Serial.print(":");
-          Serial.print(gps.time.minute());
-          Serial.print(":");
-          Serial.print(gps.time.second());
-          Serial.println();
-        }
-       
+        // Update global variables with GPS data
+        latitude = gps.location.lat();
+        longitude = gps.location.lng();
+        altitude = gps.altitude.meters();
+        course = gps.course.deg();
+        speed = gps.speed.kmph();
+        satellites = gps.satellites.value();
 
-        /* Get new sensor events with the readings */
-        sensors_event_t a, g, temp;
-        mpu.getEvent(&a, &g, &temp);
-        /* Print out the readings */
-        Serial.print("Acceleration X: ");
-        Serial.print(a.acceleration.x);
-        Serial.print(", Y: ");
-        Serial.print(a.acceleration.y);
-        Serial.print(", Z: ");
-        Serial.print(a.acceleration.z);
-        Serial.println(" m/s^2");
-        Serial.print("Rotation X: ");
-        Serial.print(g.gyro.x);
-        Serial.print(", Y: ");
-        Serial.print(g.gyro.y);
-        Serial.print(", Z: ");
-        Serial.print(g.gyro.z);
-        Serial.println(" rad/s");
-        Serial.print("Temperature: ");
-        Serial.print(temp.temperature);
-        Serial.println(" degC");
-
-
-        // Read the analog value from the microphone
-        float microphoneValue = analogRead(microphonePin);
-
-        // Print the microphone value to the serial monitor
-        Serial.print("Microphone: ");
-        Serial.println(microphoneValue);
-
-        if (client.connect(server_ip, server_port)) {
-          Serial.println("Connected to server");
-          // client.write("hello");  // Send the message to the server
-
-          // Convert the floats to byte arrays
-          byte x_bytes[sizeof(float)];
-          byte y_bytes[sizeof(float)];
-          byte z_bytes[sizeof(float)];
-          byte gyro_x_bytes[sizeof(float)];
-          byte gyro_y_bytes[sizeof(float)];
-          byte gyro_z_bytes[sizeof(float)];
-          byte temp_bytes[sizeof(float)];
-
-          memcpy(x_bytes, &a.acceleration.x, sizeof(float));
-          memcpy(y_bytes, &a.acceleration.y, sizeof(float));
-          memcpy(z_bytes, &a.acceleration.z, sizeof(float));
-          memcpy(gyro_x_bytes, &g.gyro.x, sizeof(float));
-          memcpy(gyro_y_bytes, &g.gyro.y, sizeof(float));
-          memcpy(gyro_z_bytes, &g.gyro.z, sizeof(float));
-          memcpy(temp_bytes, &temp.temperature, sizeof(float));
-
-          // Send the byte arrays to the server
-          client.write(x_bytes, sizeof(float));
-          client.write(y_bytes, sizeof(float));
-          client.write(z_bytes, sizeof(float));
-          client.write(gyro_x_bytes, sizeof(float));
-          client.write(gyro_y_bytes, sizeof(float));
-          client.write(gyro_z_bytes, sizeof(float));
-          client.write(temp_bytes, sizeof(float));
-
-
-          byte mic_bytes[sizeof(float)];
-
-          memcpy(mic_bytes, &microphoneValue, sizeof(float));
-
-          // Send the byte arrays to the server
-          client.write(mic_bytes, sizeof(float));
-
-          // Store the values in variables
-          double latitude = gps.location.lat();
-          double longitude = gps.location.lng();
-          double altitude = gps.altitude.meters();
-          double course = gps.course.deg();
-          double speed = gps.speed.kmph();
-          uint32_t satellites = gps.satellites.value();
-
-          // Convert the variables to byte arrays
-          byte lat_bytes[sizeof(double)];
-          byte long_bytes[sizeof(double)];
-          byte alt_bytes[sizeof(double)];
-          byte course_bytes[sizeof(double)];
-          byte speed_bytes[sizeof(double)];
-          byte sat_bytes[sizeof(uint32_t)];
-
-          memcpy(lat_bytes, &latitude, sizeof(double));
-          memcpy(long_bytes, &longitude, sizeof(double));
-          memcpy(alt_bytes, &altitude, sizeof(double));
-          memcpy(course_bytes, &course, sizeof(double));
-          memcpy(speed_bytes, &speed, sizeof(double));
-          memcpy(sat_bytes, &satellites, sizeof(uint32_t));
-
-
-          // Send the byte arrays to the server
-          client.write(lat_bytes, sizeof(double));
-          client.write(long_bytes, sizeof(double));
-          client.write(alt_bytes, sizeof(double));
-          client.write(course_bytes, sizeof(double));
-          client.write(speed_bytes, sizeof(double));
-          client.write(sat_bytes, sizeof(uint32_t));
-
-          client.stop();  // Disconnect from the server
-          // delay(10);
-        } else {
-          Serial.println("Connection failed");
-          delay(5000);    // Wait for 5 seconds before retrying
-        }
-
+        break;
       }
     }
+  }
+}
+
+// Function to send a variable as bytes to the server
+template <typename T>
+void sendVariableAsBytes(Client &client, T &variable) {
+  byte bytes[sizeof(T)];
+  memcpy(bytes, &variable, sizeof(T));
+  client.write(bytes, sizeof(T));
+}
+
+void sendData() {
+  if (client.connect(server_ip, server_port)) {
+    Serial.println("Connected to server");
+
+    // Send accelerometer data
+    sendVariableAsBytes(client, calibratedAccelX);
+    sendVariableAsBytes(client, calibratedAccelY);
+    sendVariableAsBytes(client, calibratedAccelZ);
+
+    // Send gyroscope data
+    sendVariableAsBytes(client, calibratedGyroX);
+    sendVariableAsBytes(client, calibratedGyroY);
+    sendVariableAsBytes(client, calibratedGyroZ);
+
+    // Send temperature data
+    sendVariableAsBytes(client, temp.temperature);
+
+    // Send microphone data
+    sendVariableAsBytes(client, microphoneValue);
+
+    // Send GPS data
+    sendVariableAsBytes(client, latitude);
+    sendVariableAsBytes(client, longitude);
+    sendVariableAsBytes(client, altitude);
+    sendVariableAsBytes(client, course);
+    sendVariableAsBytes(client, speed);
+    sendVariableAsBytes(client, satellites);
+
+    client.stop();  // Disconnect from the server
+  } else {
+    Serial.println("Connection failed");
+    delay(1000);    // Wait for 1 seconds before retrying
   }
 }
